5ce102aeb69571a9e6f22b9e48b064c22a20d40d
zndmonya <zndmonya@gmail.com>
2026-02-04 23:51:04 +0900

    ウィンドウタイトルバーにアプリ名「全文検索システム」を表示するよう修正

--- src/FileSearch.Blazor/App.xaml.cs
+++ src/FileSearch.Blazor/App.xaml.cs
@@ -11,4 +11,12 @@ public partial class App : Application
 		InitializeComponent();
 		MainPage = new MainPage();
 	}
+
+	/// <summary>ウィンドウ作成時にタイトルを設定する。Windows では ContentPage.Title がタイトルバーに反映されないためここで設定する。</summary>
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		var window = base.CreateWindow(activationState);
+		window.Title = "全文検索システム";
+		return window;
+	}
 }
